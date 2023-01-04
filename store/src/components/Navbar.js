20863430fcd8c45ce8c6668963206d14f9615a25
papacito42 <loganbcabrera@gmail.com>
2023-01-04 00:14:55 -0600

    5th try

--- store/src/components/Navbar.js
+++ store/src/components/Navbar.js
@@ -11,7 +11,7 @@ function NavbarComponent() {
     const handleShow = () => setShow(true);
 
     const checkout = async () => {
-        await fetch('http://tiendacbd.vercel.app:4000/checkout', {
+        await fetch('https://tiendacbd.vercel.app:4000/checkout', {
             method: "POST",
             headers: {
                 'Content-Type': 'application/json'
